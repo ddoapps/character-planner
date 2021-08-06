c0bc2183531e6836364fa694e9381e0724b69d4f
taplar <taplarhw@gmail.com>
2021-08-06 16:03:47 -0500

    Make a build script that will merge css and html files into the ts files.

--- projects/app/src/components/app-root.component.ts
+++ projects/app/src/components/app-root.component.ts
@@ -1,23 +1,9 @@
 import { Component } from "../framework/component";
 
-const styles = `
-    span {
-        background-color: rgba(var(--app-color-black-rgb), 0.90);
-        min-height: 640px;
-        height: 100vh;
-        min-width: 360px;
-        width: 100vw;
-    }
-`;
-
-const template = `
-    <div>Hello</div>
-`;
-
 @Component({
     tagName: 'app-root',
-    styles,
-    template
+    styles: 'app-root.component.css',
+    template: 'app-root.component.html'
 })
 export class AppRootComponent extends HTMLElement {
     constructor () {
